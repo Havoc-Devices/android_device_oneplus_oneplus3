e655b6c2a8fbc3c8542c9f0614aaab6f8b9f6a91
Anushek Prasal <anushekprasal@gmail.com>
2020-02-22 21:06:00 +0530

    op3: Add OnePlus Camera & Gallery
    Signed-off-by: Anushek Prasal <anushekprasal@gmail.com>

--- device.mk
+++ device.mk
@@ -403,6 +403,16 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
 
+# Prebuilts
+PRODUCT_PACKAGES += \
+    oneplus-framework-res \
+    OnePlusCamera \
+    OnePlusCameraService \
+    OnePlusGallery
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/prebuilts/etc/permissions/privapp-permissions-oem-system.xml:system/etc/permissions/privapp-permissions-oem-system.xml
+
 # Protobuf
 PRODUCT_COPY_FILES += \
     prebuilts/vndk/v28/arm64/arch-arm-armv8-a/shared/vndk-core/libprotobuf-cpp-lite.so:$(TARGET_COPY_OUT_VENDOR)/lib/libprotobuf-cpp-lite-v28.so \
